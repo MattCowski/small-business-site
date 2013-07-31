35643f2bccb84403df4e6819b12020bd83ce5052
Matt Cien <mattcien@gmail.com>
2013-07-31 16:31:06 -0500

    changed secret_token and added rspec

--- Gemfile
+++ Gemfile
@@ -4,13 +4,14 @@ ruby '2.0.0'
 
 gem 'rails', '4.0.0'
 
-group :development do
+group :development, :test do
   gem 'sqlite3', '1.3.7'
+  gem 'rspec-rails', '2.13.1'
 end
 
-group :production do
-  gem 'pg', '0.15.1'
-  gem 'rails_12factor', '0.0.2'
+group :test do
+  gem 'selenium-webdriver', '2.0.0'
+  gem 'capybara', '2.1.0'
 end
 
 gem 'sass-rails', '4.0.0'
@@ -24,6 +25,10 @@ group :doc do
   gem 'sdoc', '0.3.20', require: false
 end
 
+group :production do
+  gem 'pg', '0.15.1'
+  gem 'rails_12factor', '0.0.2'
+end
 # #ABOVE IS HARTLE TUTORIAL
 # source 'https://rubygems.org'
 
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,3 +1,4 @@
+
 # Be sure to restart your server when you modify this file.
 
 # Your secret key is used for verifying the integrity of signed cookies.
@@ -9,4 +10,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-KTSTony::Application.config.secret_key_base = 'e3ea655c87ec0467a905f46bd6a22ea96c8025ebebd59ec57ce3a032812a8e5753bc9b015eb7c8bc3ecfada544f9849adce1a8290a904d172c484168581e8a76'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+KTSTony::Application.config.secret_key_base = secure_token
