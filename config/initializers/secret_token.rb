b9f38e1e957dcac3bf632aa8231fd45f7b933a81
Matt Cien <mattcien@gmail.com>
2013-07-31 13:38:46 -0500

    locked in gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KTSTony::Application.config.secret_key_base = 'e3ea655c87ec0467a905f46bd6a22ea96c8025ebebd59ec57ce3a032812a8e5753bc9b015eb7c8bc3ecfada544f9849adce1a8290a904d172c484168581e8a76'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KTSTony::Application.config.session_store :cookie_store, key: '_KTS_Tony_session'
