fe38ce627524f984bf70192618984b085f1b9322
Matt Cien <mattcien@gmail.com>
2013-08-01 14:57:34 -0500

    fixed rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 KTSTony::Application.routes.draw do
+  get "static_pages/about"
+  get "static_pages/contact"
   get "static_pages/home"
   get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
